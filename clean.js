0b7627ba90c67e990b5b96a90187453352d6c464
Ivan Telychkin <Ivan.Telichkin@Edetek.com>
2019-04-09 10:08:40 +0300

    Made some changes

--- clean.js
+++ clean.js
@@ -1,10 +1,10 @@
-var fs = require('fs');
+const fs = require('fs');
 
 function deleteFolderRecursive(path) {
   if (fs.existsSync(path) && fs.lstatSync(path).isDirectory()) {
-    fs.readdirSync(path).forEach(function(file, index){
-      var curPath = path + "/" + file;
-
+    fs.readdirSync(path).forEach(function (file, index) {
+      const curPath = path + "/" + file;
+      console.log(curPath)
       if (fs.lstatSync(curPath).isDirectory()) { // recurse
         deleteFolderRecursive(curPath);
       } else { // delete file
